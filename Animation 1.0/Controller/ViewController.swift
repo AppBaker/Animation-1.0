2976ef152e6b863f2d729664a37d099966625452
Ivan Nikitin <qqaqq@ya.ru>
2019-05-23 16:58:05 +0700

    Leg move

--- Animation 1.0/Controller/ViewController.swift
+++ Animation 1.0/Controller/ViewController.swift
@@ -13,79 +13,87 @@ class ViewController: UIViewController {
     @IBOutlet weak var leftLegImageView: UIImageView!
     @IBOutlet weak var rightLegImageView: UIImageView!
     @IBOutlet weak var startButton: UIButton!
-    @IBOutlet weak var countLabel: UILabel!
-    
-    
-    var numberOfexercise = 0
-//    var square: UIView!
-//    let initialFrame = CGRect(x: 0, y: 0, width: 100, height: 100)
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        startButton.layer.cornerRadius = 20
-        startButton.clipsToBounds = true
-        
-//        square = UIView(frame: initialFrame)
-//        square.backgroundColor = .purple
-//        view.addSubview(square)
+    @IBOutlet weak var moveLeftButton: UIButton!
+    @IBOutlet weak var moveRightButton: UIButton!
+
+
+    @IBAction func startButtonTapped(_ sender: Any) {
+        startButton.isEnabled = false
+            UIView.animate(withDuration: 0.12, animations: {
+                let scaleTransform = CGAffineTransform(scaleX: 1.2, y: 1.2)
+                self.startButton.transform = scaleTransform
+            }) { _ in
+                UIView.animate(withDuration: 0.06, animations: {
+                    self.startButton.transform = CGAffineTransform.identity
+                    self.startButton.setImage(#imageLiteral(resourceName: "pause"), for: [])
+                })
+            }
+        legsAnimation()
     }
     
-//    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-//
-//        let width = view.frame.width
-//        let height = view.frame.height
-//
-//        numberOfTouches += 1
-//
-//        switch numberOfTouches {
-//        case 1:
-//            let scaleTransform = CGAffineTransform(scaleX: 2, y: 2)
-//            let rotateTransform = CGAffineTransform(rotationAngle: .pi)
-//            let translateTransform = CGAffineTransform(translationX: 200, y: 200)
-//
-//            let comboTransform = scaleTransform.concatenating(
-//                rotateTransform.concatenating(translateTransform)
-//            )
-//
-//            UIView.animate(withDuration: 2) {
-//                self.leftLegImageView.transform = comboTransform
-//            }
-//
-//
-//        case 2:
-//            UIView.animate(withDuration: 1, animations: {
-//                self.square.backgroundColor = .green
-//                self.square.frame = CGRect(x: 150, y: 150, width: 200, height: 200)
-//            }) { _ in
-//                UIView.animate(withDuration: 0.5) {
-//                    self.square.backgroundColor = .purple
-//                    self.square.frame = self.initialFrame
-//                }
-//            }
-//
-//        case 3:
-//            UIView.animate(withDuration: 3, delay: 1, options: [.autoreverse ,.repeat], animations: {
-//                self.square.frame = CGRect(x: width / 2, y: height / 2, width: 20, height: 300)
-//                self.square.backgroundColor = .green
-//            }) { bool in
-//                self.view.backgroundColor = .gray
-//            }
-//        default:
-//            numberOfTouches = 0
-//        }
-//    }
-    
-    
-    @IBAction func startButtonTapped(_ sender: Any) {
+    @IBAction func moveLeftButtonTapped(_ sender: UIButton) {
+
+        UIView.animate(withDuration: 0.12, animations: {
+            let scaleTransform = CGAffineTransform(scaleX: 1.2, y: 1.2)
+            sender.transform = scaleTransform
+        }) { _ in
+            UIView.animate(withDuration: 0.06, animations: {
+                sender.transform = CGAffineTransform.identity
+            })
+        }
+        legsAnimationIn()
+    }
+    @IBAction func moveRightButtonTapped(_ sender: UIButton) {
         
-        startButton.isEnabled = false
+        UIView.animate(withDuration: 0.12, animations: {
+            let scaleTransform = CGAffineTransform(scaleX: 1.2, y: 1.2)
+            sender.transform = scaleTransform
+        }) { _ in
+            UIView.animate(withDuration: 0.06, animations: {
+                sender.transform = CGAffineTransform.identity
+            })
+        }
+        legsAnimationOut()
+    }
+    
+    //MARK: - Animations
+    func legsAnimationOut() {
+        //Left leg transform
+        let rotateTransformLeftLeg = CGAffineTransform(rotationAngle: .pi / 8)
+        let translateTransformLeftLeg = CGAffineTransform(translationX: -35, y: 0)
+        let comboTransformLeftLeg = rotateTransformLeftLeg.concatenating(translateTransformLeftLeg)
+        
+        //Right leg transform
+        let rotateTransformRightLeg = CGAffineTransform(rotationAngle: -.pi / 8)
+        let translateTransformRightLeg = CGAffineTransform(translationX: 35, y: 0)
+        let comboTransformRightLeg = rotateTransformRightLeg.concatenating(translateTransformRightLeg)
+        
+        let animator = UIViewPropertyAnimator(duration: 1, curve: .linear) {
+            print("Animation")
+            self.leftLegImageView.transform = comboTransformLeftLeg
+            self.rightLegImageView.transform = comboTransformRightLeg
+        }
+        animator.startAnimation()
+    }
+    
+    func legsAnimationIn() {
+        
+        let animator = UIViewPropertyAnimator(duration: 1, curve: .linear) {
+            print("Animation")
+            self.leftLegImageView.transform = CGAffineTransform.identity
+            self.rightLegImageView.transform = CGAffineTransform.identity
+        }
+        animator.startAnimation()
+    }
+    
+    func legsAnimation() {
         
         //Left leg transform
         let rotateTransformLeftLeg = CGAffineTransform(rotationAngle: .pi / 8)
         let translateTransformLeftLeg = CGAffineTransform(translationX: -35, y: 0)
         let comboTransformLeftLeg = rotateTransformLeftLeg.concatenating(translateTransformLeftLeg)
         
-       //Right leg transform
+        //Right leg transform
         let rotateTransformRightLeg = CGAffineTransform(rotationAngle: -.pi / 8)
         let translateTransformRightLeg = CGAffineTransform(translationX: 35, y: 0)
         let comboTransformRightLeg = rotateTransformRightLeg.concatenating(translateTransformRightLeg)
@@ -101,9 +109,7 @@ class ViewController: UIViewController {
             }, completion: { _ in
                 print("Done")
                 self.startButton.isEnabled = true
-                self.numberOfexercise += 1
-                self.countLabel.text = "count: \(self.numberOfexercise)"
-                
+                    self.startButton.setImage(#imageLiteral(resourceName: "playSolid"), for: [])
             })
         }
     }
